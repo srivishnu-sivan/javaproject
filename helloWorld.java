6bcc2c12e4ff59b922e53ad3170aeb7a9ec6a197
srivishnu-sivan <mallikasrivishnu@gmail.com>
2024-11-09 12:40:48 +0530

    to trigger a job aftera java project

--- helloWorld.java
+++ helloWorld.java
@@ -3,7 +3,7 @@
 class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
-        System.out.println("Hello, World 22!");
+        System.out.println("Hello, World 23!");
         
     }
 }
